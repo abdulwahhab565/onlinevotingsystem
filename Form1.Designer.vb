e28213d45cc94610edc69c2c4eb9a31f929b861c
Abdul Wahhab Mohammed <146930793+abdulwahhab565@users.noreply.github.com>
2025-03-20 15:02:20 +0000

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.can1 = New System.Windows.Forms.Label()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.can2 = New System.Windows.Forms.Label()
+        Me.tvote = New System.Windows.Forms.Label()
+        Me.winner = New System.Windows.Forms.Label()
+        Me.can1r = New System.Windows.Forms.Label()
+        Me.can2r = New System.Windows.Forms.Label()
+        Me.tvoter = New System.Windows.Forms.Label()
+        Me.winnerr = New System.Windows.Forms.Label()
+        Me.can1s = New System.Windows.Forms.Label()
+        Me.can2s = New System.Windows.Forms.Label()
+        Me.tvotes = New System.Windows.Forms.Label()
+        Me.winners = New System.Windows.Forms.Label()
+        Me.can1sr = New System.Windows.Forms.Label()
+        Me.can2sr = New System.Windows.Forms.Label()
+        Me.tvotesr = New System.Windows.Forms.Label()
+        Me.winnersr = New System.Windows.Forms.Label()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel4 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel5 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(341, 18)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(113, 13)
+        Me.LinkLabel1.TabIndex = 0
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "ELECTION RESULTS"
+        '
+        'can1
+        '
+        Me.can1.AutoSize = True
+        Me.can1.Location = New System.Drawing.Point(268, 85)
+        Me.can1.Name = "can1"
+        Me.can1.Size = New System.Drawing.Size(39, 13)
+        Me.can1.TabIndex = 1
+        Me.can1.Text = "Label1"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(202, 47)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(150, 13)
+        Me.LinkLabel2.TabIndex = 2
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "PRESIDENTIAL CANDIDATE"
+        '
+        'can2
+        '
+        Me.can2.AutoSize = True
+        Me.can2.Location = New System.Drawing.Point(268, 120)
+        Me.can2.Name = "can2"
+        Me.can2.Size = New System.Drawing.Size(39, 13)
+        Me.can2.TabIndex = 3
+        Me.can2.Text = "Label2"
+        '
+        'tvote
+        '
+        Me.tvote.AutoSize = True
+        Me.tvote.Location = New System.Drawing.Point(268, 157)
+        Me.tvote.Name = "tvote"
+        Me.tvote.Size = New System.Drawing.Size(39, 13)
+        Me.tvote.TabIndex = 4
+        Me.tvote.Text = "Label3"
+        '
+        'winner
+        '
+        Me.winner.AutoSize = True
+        Me.winner.Location = New System.Drawing.Point(268, 199)
+        Me.winner.Name = "winner"
+        Me.winner.Size = New System.Drawing.Size(39, 13)
+        Me.winner.TabIndex = 5
+        Me.winner.Text = "Label4"
+        '
+        'can1r
+        '
+        Me.can1r.AutoSize = True
+        Me.can1r.Location = New System.Drawing.Point(415, 85)
+        Me.can1r.Name = "can1r"
+        Me.can1r.Size = New System.Drawing.Size(39, 13)
+        Me.can1r.TabIndex = 6
+        Me.can1r.Text = "Label5"
+        '
+        'can2r
+        '
+        Me.can2r.AutoSize = True
+        Me.can2r.Location = New System.Drawing.Point(415, 120)
+        Me.can2r.Name = "can2r"
+        Me.can2r.Size = New System.Drawing.Size(39, 13)
+        Me.can2r.TabIndex = 7
+        Me.can2r.Text = "Label6"
+        '
+        'tvoter
+        '
+        Me.tvoter.AutoSize = True
+        Me.tvoter.Location = New System.Drawing.Point(415, 157)
+        Me.tvoter.Name = "tvoter"
+        Me.tvoter.Size = New System.Drawing.Size(39, 13)
+        Me.tvoter.TabIndex = 8
+        Me.tvoter.Text = "Label7"
+        '
+        'winnerr
+        '
+        Me.winnerr.AutoSize = True
+        Me.winnerr.Location = New System.Drawing.Point(415, 199)
+        Me.winnerr.Name = "winnerr"
+        Me.winnerr.Size = New System.Drawing.Size(39, 13)
+        Me.winnerr.TabIndex = 9
+        Me.winnerr.Text = "Label8"
+        '
+        'can1s
+        '
+        Me.can1s.AutoSize = True
+        Me.can1s.Location = New System.Drawing.Point(268, 259)
+        Me.can1s.Name = "can1s"
+        Me.can1s.Size = New System.Drawing.Size(39, 13)
+        Me.can1s.TabIndex = 10
+        Me.can1s.Text = "Label9"
+        '
+        'can2s
+        '
+        Me.can2s.AutoSize = True
+        Me.can2s.Location = New System.Drawing.Point(268, 299)
+        Me.can2s.Name = "can2s"
+        Me.can2s.Size = New System.Drawing.Size(45, 13)
+        Me.can2s.TabIndex = 11
+        Me.can2s.Text = "Label10"
+        '
+        'tvotes
+        '
+        Me.tvotes.AutoSize = True
+        Me.tvotes.Location = New System.Drawing.Point(268, 341)
+        Me.tvotes.Name = "tvotes"
+        Me.tvotes.Size = New System.Drawing.Size(45, 13)
+        Me.tvotes.TabIndex = 12
+        Me.tvotes.Text = "Label11"
+        '
+        'winners
+        '
+        Me.winners.AutoSize = True
+        Me.winners.Location = New System.Drawing.Point(268, 382)
+        Me.winners.Name = "winners"
+        Me.winners.Size = New System.Drawing.Size(45, 13)
+        Me.winners.TabIndex = 13
+        Me.winners.Text = "Label12"
+        '
+        'can1sr
+        '
+        Me.can1sr.AutoSize = True
+        Me.can1sr.Location = New System.Drawing.Point(415, 259)
+        Me.can1sr.Name = "can1sr"
+        Me.can1sr.Size = New System.Drawing.Size(45, 13)
+        Me.can1sr.TabIndex = 14
+        Me.can1sr.Text = "Label13"
+        '
+        'can2sr
+        '
+        Me.can2sr.AutoSize = True
+        Me.can2sr.Location = New System.Drawing.Point(415, 299)
+        Me.can2sr.Name = "can2sr"
+        Me.can2sr.Size = New System.Drawing.Size(45, 13)
+        Me.can2sr.TabIndex = 15
+        Me.can2sr.Text = "Label14"
+        '
+        'tvotesr
+        '
+        Me.tvotesr.AutoSize = True
+        Me.tvotesr.Location = New System.Drawing.Point(415, 341)
+        Me.tvotesr.Name = "tvotesr"
+        Me.tvotesr.Size = New System.Drawing.Size(45, 13)
+        Me.tvotesr.TabIndex = 16
+        Me.tvotesr.Text = "Label15"
+        '
+        'winnersr
+        '
+        Me.winnersr.AutoSize = True
+        Me.winnersr.Location = New System.Drawing.Point(415, 382)
+        Me.winnersr.Name = "winnersr"
+        Me.winnersr.Size = New System.Drawing.Size(45, 13)
+        Me.winnersr.TabIndex = 17
+        Me.winnersr.Text = "Label16"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(399, 47)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(77, 13)
+        Me.LinkLabel3.TabIndex = 18
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "VOTE COUNT"
+        '
+        'LinkLabel4
+        '
+        Me.LinkLabel4.AutoSize = True
+        Me.LinkLabel4.Location = New System.Drawing.Point(202, 230)
+        Me.LinkLabel4.Name = "LinkLabel4"
+        Me.LinkLabel4.Size = New System.Drawing.Size(137, 13)
+        Me.LinkLabel4.TabIndex = 19
+        Me.LinkLabel4.TabStop = True
+        Me.LinkLabel4.Text = "SECRETARY CANDIDATE"
+        '
+        'LinkLabel5
+        '
+        Me.LinkLabel5.AutoSize = True
+        Me.LinkLabel5.Location = New System.Drawing.Point(399, 230)
+        Me.LinkLabel5.Name = "LinkLabel5"
+        Me.LinkLabel5.Size = New System.Drawing.Size(77, 13)
+        Me.LinkLabel5.TabIndex = 20
+        Me.LinkLabel5.TabStop = True
+        Me.LinkLabel5.Text = "VOTE COUNT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LinkLabel5)
+        Me.Controls.Add(Me.LinkLabel4)
+        Me.Controls.Add(Me.LinkLabel3)
+        Me.Controls.Add(Me.winnersr)
+        Me.Controls.Add(Me.tvotesr)
+        Me.Controls.Add(Me.can2sr)
+        Me.Controls.Add(Me.can1sr)
+        Me.Controls.Add(Me.winners)
+        Me.Controls.Add(Me.tvotes)
+        Me.Controls.Add(Me.can2s)
+        Me.Controls.Add(Me.can1s)
+        Me.Controls.Add(Me.winnerr)
+        Me.Controls.Add(Me.tvoter)
+        Me.Controls.Add(Me.can2r)
+        Me.Controls.Add(Me.can1r)
+        Me.Controls.Add(Me.winner)
+        Me.Controls.Add(Me.tvote)
+        Me.Controls.Add(Me.can2)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.can1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents can1 As Label
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents can2 As Label
+    Friend WithEvents tvote As Label
+    Friend WithEvents winner As Label
+    Friend WithEvents can1r As Label
+    Friend WithEvents can2r As Label
+    Friend WithEvents tvoter As Label
+    Friend WithEvents winnerr As Label
+    Friend WithEvents can1s As Label
+    Friend WithEvents can2s As Label
+    Friend WithEvents tvotes As Label
+    Friend WithEvents winners As Label
+    Friend WithEvents can1sr As Label
+    Friend WithEvents can2sr As Label
+    Friend WithEvents tvotesr As Label
+    Friend WithEvents winnersr As Label
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents LinkLabel4 As LinkLabel
+    Friend WithEvents LinkLabel5 As LinkLabel
+End Class
